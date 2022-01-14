c7eced7b1023366085ac91b597f063d495196f9f
Jayant Kodwani <JAYANT.KODWANI@YAHOO.IN>
2022-01-14 09:42:23 +0530

    Create Libraries.py

---
+++ Libraries.py
@@ -0,0 +1,14 @@
+pip install pandas
+
+pip install django
+
+pip install tk
+pip install opencv-python
+pip install MySQL-python
+
+python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+
+python -m pip install -U pip
+python -m pip install -U matplotlib
+
+pip install seaborn
